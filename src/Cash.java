ebdcf18e15350a4367ae89f1787952a11b734c08
colemanA1 <coleman.r.alex@gmail.com>
2017-02-15 13:25:16 -0500

    Enhanced Cash Class

--- src/Cash.java
+++ src/Cash.java
@@ -1,5 +1,6 @@
-
+import java.util.*;
 public class Cash {
+    static Scanner scan = new Scanner(System.in);
     /**
      * Ask user for:
      * amount tender received
@@ -9,4 +10,17 @@ public class Cash {
      * print out cash to be changed to customer
      *send to receipt class
      */
+public static double cashGoingin (){
+   double cashChanged = Validator.getDouble(scan, "Enter amount of cash given: ");
+   double cashOut = cashChanged - Payment.total.;
+   System.out.println("Press ENTER to Commit Transaction: ");
+   String transactionCommit = scan.nextLine();
+   if (transactionCommit.equalsIgnoreCase("")) {
+       System.out.println("Change total: " + cashOut);
+       return cashOut;
+   }else 
+}
+
+
+
 }

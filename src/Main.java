9bb6823821286ef192d0148ec2d0c6def435ae54
mwithers313 <mikewithers313@gmail.com>
2017-02-15 14:24:16 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
+import sun.misc.JavaSecurityProtectionDomainAccess;
+
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    public static void newTextTofile(String fileName, String Input) { //this method is a write text to file method but append is FALSE (
 
-    public static void writeTextToFile(String fileName, String Input) {
+
+        Path filePath = Paths.get(fileName); //step number 1. Start with a path
+
+
+        File productsFile = filePath.toFile(); //step number 2. Create a file path
+
+
+        try {
+            PrintWriter out = new PrintWriter(new FileOutputStream(productsFile, true));
+
+
+            out.print(Input);
+
+            out.close(); // this is how we close
+
+        } catch (FileNotFoundException ex) {
+
+        }
+
+    }
+
+
+    public static void writeTextToFile(String fileName, ArrayList<Product> productArrayList) {
 
 
         Path filePath = Paths.get(fileName);
@@ -16,7 +42,7 @@ public class Main {
         try {
             PrintWriter out = new PrintWriter(new FileOutputStream(productsFile, true));
 
-            out.print(Input);
+            out.print(productArrayList);
 
             out.close();
 
@@ -52,22 +78,100 @@ public class Main {
         }
     }
 
+
+    public static ArrayList<Product> findCategory(String input, ArrayList<Product> productArrayList) {
+
+        ArrayList<Product> ProductByItemNumber = new ArrayList<Product>();
+
+        for (int i = 0; i < productArrayList.size(); i++) {
+
+            if (input.equalsIgnoreCase(productArrayList.get(i).getItemNumber())) {
+
+                ProductByItemNumber.add(productArrayList.get(i));
+            }
+        }
+        return ProductByItemNumber;
+    }
+
+
+    public static boolean anotherItem() { //play again loop method
+
+        Scanner scnr = new Scanner(System.in);
+        String userInput = "";
+        char answer = '-';
+
+        System.out.println("Another Item? (y/n):");
+
+        userInput = scnr.next();
+        answer = userInput.charAt(0);
+
+        if ((answer == 'Y') || (answer == 'y')) {
+            return true;
+
+        } else if ((answer == 'N') || (answer == 'n')) {
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        writeTextToFile("ProductList", "Product: Bananas\tCategory: Fruit\t \nChicken\nAvocado\nGround Turkey\nBread\nCereal\nMilk\nOrange Juice\nCheese\nOatmeal\nM&M's\nDoughnut\nCream Cheese"); //this adds the original items to the list
+        Scanner scan = new Scanner(System.in);
 
-        ArrayList<Product> foodList = new ArrayList<Product>();
+        ArrayList<Product> productArrayList = new ArrayList<Product>();
+
+        System.out.println("WELCOME TO THE ALL-STAR FOOD MENU!!!");
+        productArrayList.add(new Product("1", "Banana", "Fruit", "Fruit", 2.00));
+        productArrayList.add(new Product("2", "Chicken", "Meat", "Meat", 2.00));
+        productArrayList.add(new Product("3", "Avocado", "Fruit", "Fruit", 2.00));
+        productArrayList.add(new Product("4", "Ground Turkey", "Meat", "Fruit", 2.00));
+        productArrayList.add(new Product("5", "Wheat Bread", "Bread", "Loaf", 2.00));
+        productArrayList.add(new Product("6", "Cereal", "Breakfast", "Food", 2.00));
+        productArrayList.add(new Product("7", "Cheese", "Dairy", "Dairy", 2.00));
+        productArrayList.add(new Product("8", "Oatmeal", "Breakfast", "Oats", 2.00));
+        productArrayList.add(new Product("9", "M&M's", "Category", "Candy", 2.00));
+        productArrayList.add(new Product("10", "Milk", "Drink", "Dairy", 2.00));
+        productArrayList.add(new Product("11", "Steak", "Meat", "Cow", 2.00));
+        productArrayList.add(new Product("12", "Noodles", "Noodles", "Rice Noodles", 2.00));
+
+        writeTextToFile("ProductList.txt", productArrayList);
 
 
-        foodList.add(
+        System.out.println("1. Banana\n" + "2. Chicken\n" + "3. Avocado\n" + "4. Ground Turkey\n" + "5. Wheat Bead\n" +
+                "6. Cereal\n" + "7. Cheese\n" + "8. Oatmeal\n" + "10. Milk\n" + "11. Steak\n" + "12. Noodles\n");
 
-        try {
-            FileOutputStream fos = new FileOutputStream("ProductList");
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(foodList); // write MenuArray to ObjectOutputStream
-            oos.close();
-        } catch(Exception ex) {
-            ex.printStackTrace();
+        //we need to add category, description, and price
+
+
+        boolean anotherPurchase = true;
+        while (anotherPurchase) {
+            System.out.println("Please select the item number you would like to purchase?:");
+
+            String itemSelected = scan.next();
+
+            System.out.println("How many would you like to purchase?");
+
+            int amountPurchased = scan.nextInt();
+
+            String item = "";
+
+            System.out.println(findCategory(itemSelected, productArrayList));
+
+            anotherPurchase = anotherItem();
+
+
         }
 
+
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by Michael on 2/15/2017.
  */
@@ -7,46 +9,63 @@ public class Product {
     private String category;
     private String description;
     private double price;
+    private String itemNumber;
 
-    public Product(String name, String category, String description, double price) {
+
+    public Product(String itemNumber, String name, String category, String description, double price) {
+        this.itemNumber = itemNumber;
         this.name = name;
         this.category = category;
         this.description = description;
         this.price = price;
-    }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getCategory() {
-            return category;
-        }
-
-        public void setCategory(String category) {
-            this.category = category;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public double getPrice() {
-            return price;
-        }
-
-        public void setPrice(double price) {
-            this.price = price;
-        }
-
 
     }
+
+    public String getItemNumber() {
+        return itemNumber;
+    }
+
+    public void setItemNumber(String itemNumber) {
+        this.itemNumber = itemNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+
+        return name + "\t" + category + "\t" + description + "\t" + price;
+
+    }
+}
 

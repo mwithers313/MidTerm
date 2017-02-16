a750f671758362a8307eea756325b8fc426787e9
colemanA1 <coleman.r.alex@gmail.com>
2017-02-16 10:00:35 -0500

    updated Payment

--- src/Payment.java
+++ src/Payment.java
@@ -1,30 +1,37 @@
 import java.util.*;
 import java.util.concurrent.Callable;
 
-/*public class Payment {
+public class Payment {
+static Scanner scan = new Scanner(System.in);
 
-    /**
-     * Total method
-     * Pulls subtotal from main
-     * calculate subtotal * tax
-     * spit out total amount
-     * Ask for payment option
-     */
 
-    /**payment options
-     * 1. Cash
-     * 2. Credit
-     * 3. Check
-     *********/
 
-    /**
-     * payment option will pull from payment classes
-     *
-     */
+    public double cashGoingIn(double total) {
+        double cashChanged = Validator.getDouble(scan, "Enter amount of cash given: ");
+        double cashOut = cashChanged - total;
+        Validator.userExit(scan, "Press ENTER to Commit Transaction");
+        return cashOut;
+    }
+    public static double checkGoingIn(double total ){
+        int checkNumber = Validator.getInt(scan, "Enter check number: ");
+        Boolean transactionCommit = Validator.userExit(scan, "Press ENTER to Commit Transaction");
+        return total;
+    }
+    public static double creditInfo(double total){
+        double cashout = total;
+        Boolean complete = true;
+        while (complete) {
+            int enterCardNumber = Validator.getInt(scan, "Enter Credit Card Number: ", 16);
 
-   /* public static Cash cashPayment = new Cash();
-    public static Check checkPayment = new Check();
-    public static Credit creditPayment = new Credit();
+            int enterDate = Validator.isDate(scan, "Enter Date: ", 4);
+            int enterCVV = Validator.isCVV(scan, "Enter CVV num: ", 3);
+            System.out.println("Complete Transaction? (y/n)");
+            if (scan.hasNext("y")) {
+                complete = false;
+            }
+        }
+        return total;
+    }
 
     public static double calculateTax (double subtotal) {
 
@@ -33,26 +40,27 @@ import java.util.concurrent.Callable;
         tax = subtotal * .06;
         return tax;
     }
+    public static double calcualteTotal(double sum){
+        double total = 0;
+        total = sum * 1.06;
+        return total;
+    }
 
 
-    public calculatePayment(double subtotal) {
+    public String paymentMethod(double subtotal) {
 
         Scanner scan1 = new Scanner(System.in);
-        String inputPaymentType = " ";
+        int inputPaymentType = 0;
 
-        System.out.println("How do you want to pay? Case / Credit / Check");
-        inputPaymentType = scan1.nextLine();
+        System.out.println("How do you want to pay?\n1.Cash\n 2.Credit \n3.Check");
+        inputPaymentType = scan1.nextInt();
 
-        if (inputPaymentType.equalsIgnoreCase("cash") {
-            Cash.cashGoingin();
-        }
+        if(inputPaymentType == 1 ){
+            double CashOut = cashGoingIn();
+            cashGoingIn();
 
-        else if (inputPaymentType.equalsIgnoreCase("credit") {
-                Credit.();
 
 
             }
             }
         }
-
-        */
